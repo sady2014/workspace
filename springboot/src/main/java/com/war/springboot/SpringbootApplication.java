fd0a9f0eca30f2f6dc367f2b3bdc25616a051ae9
sady2014 <wanglu2325@qq.com>
2018-06-16 06:28:48 +0800

    提交程序入口

---
+++ springboot/src/main/java/com/war/springboot/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.war.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
